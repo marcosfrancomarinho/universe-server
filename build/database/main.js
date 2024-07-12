1c3f7a87e5b20375a19f07029edc5462acdc0f35
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-07-12 19:47:21 -0300

    rota update  de planetas finalizada

--- build/database/main.js
+++ build/database/main.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('postgres://gsuhelcb:HZ17wkhBtEprABlbFX_CTriLvh2oiWNt@isabelle.db.elephantsql.com/gsuhelcb', {
+const sequelize = new sequelize_1.Sequelize(process.env.URL, {
     dialect: 'postgres'
 });
 exports.default = sequelize;
